37424410730a7f5954930a4485bf2bdbcd1f68a9
garima94921 <gaarimabothra@gmail.com>
2020-07-31 22:36:32 +0530

    Added filter types

--- Landmarks/View/Filter.swift
+++ Landmarks/View/Filter.swift
@@ -10,9 +10,18 @@ import SwiftUI
 
 struct Filter: View {
     @EnvironmentObject private var userData: UserData
+    @Binding var filter: FilterType
 
     var body: some View {
         HStack {
+            Picker(selection: $filter, label: EmptyView()) {
+                ForEach(FilterType.allCases) { choice in
+                    Text(choice.name).tag(choice)
+                }
+            }
+
+            Spacer()
+
              Toggle(isOn: $userData.showFavoritesOnly) {
                  Text("Favorites only")
              }
@@ -22,12 +31,12 @@ struct Filter: View {
 
 struct Filter_Previews: PreviewProvider {
     static var previews: some View {
-        Filter()
+        Filter(filter: .constant(.all))
             .environmentObject(UserData())
     }
 }
 
-struct FilterType: Hashable {
+struct FilterType: CaseIterable, Hashable, Identifiable {
     var name: String
     var category: Landmark.Category?
 
@@ -35,11 +44,20 @@ struct FilterType: Hashable {
         self.name = category.rawValue
         self.category = category
     }
-    
+
     init(name: String) {
         self.name = name
         self.category = nil
     }
 
     static var all = FilterType(name: "All")
+
+    static var allCases: [FilterType] {
+        return [.all] + Landmark.Category.allCases.map(FilterType.init)
+    }
+
+    var id: FilterType {
+        return self
+    }
+
 }

f67037120e87f1dd731140a7ede43b60c86bf097
garima94921 <gaarimabothra@gmail.com>
2020-07-31 22:10:42 +0530

    Added ratings to landmark

--- Landmarks/View/LandmarkRow.swift
+++ Landmarks/View/LandmarkRow.swift
@@ -12,7 +12,35 @@ struct LandmarkRow: View {
     var landmark: Landmark
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       HStack(alignment: .center) {
+            landmark.image
+                .resizable()
+                .aspectRatio(1.0, contentMode: .fit)
+                .frame(width: 32, height: 32)
+                .fixedSize(horizontal: true, vertical: false)
+                .cornerRadius(4.0)
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .fontWeight(.bold)
+                    .truncationMode(.tail)
+                    .frame(minWidth: 20)
+
+                Text(landmark.park)
+                    .font(.caption)
+                    .opacity(0.625)
+                    .truncationMode(.middle)
+            }
+        Spacer()
+
+        if landmark.isFavorite {
+           Image("star-filled")
+               .resizable()
+               .renderingMode(.template)
+               .foregroundColor(.yellow)
+               .frame(width: 10, height: 10)
+            .padding([.vertical,.trailing], 4)
+        }
+        }
     }
 }
 
